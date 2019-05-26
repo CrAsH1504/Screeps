9cbb0a6ccdde548139954c214f11809507325a12
CrAsH <kornilov_vad@mail.ru>
2019-05-26 22:59:26 +0300

    add test

--- develop/main.js
+++ develop/main.js
@@ -1,19 +1,49 @@
-var roleHarvester = require('role.harvester');
+/*var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
+*/
+
+var start = require('start.creep');
+
+
+const harvesterMax = 4;
+
 
 module.exports.loop = function () {
+     // console.log( " start " + Game.cpu.getUsed())
+      var cpu =  Game.cpu.getUsed()
 
+    
+  /*  for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }*/
+   var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvest');
+    
+    if(harvesters.length < harvesterMax) {
+        var newName = 'Harvester' + Game.time;
+        console.log('Spawning new harvester: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([MOVE,CARRY,WORK], newName, 
+            {memory: {role: 'harvest'}});        
+    }
+
+    start.run();
+/*
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
+       //     roleHarvester.run(creep);
         }
         if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
+        //    roleUpgrader.run(creep);
         }
         if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
+        //    roleBuilder.run(creep);
         }
-    }
+    }*/
+    //console.log( " end " + Game.cpu.getUsed())
+        console.log(  Game.cpu.getUsed() - cpu)
+
 }
